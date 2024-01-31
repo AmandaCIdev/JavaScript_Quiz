fccd6382b4d78ac4cc22a06762ce1264fe71d09f
AmandaCIdev M <amandamascurinedev@gmail.com>
2024-01-31 20:55:02 +0000

    Add: score counter and button to replay for user

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const questions = [{
                 correct: true
             },
             {
-                text: "<!-- comment --> ",
+                text: "<!-- comment -->",
                 correct: false
             },
         ]
@@ -248,9 +248,41 @@ function selectAnswer(e) {
     const isCorrect = selectedBtn.dataset.correct === "true";
     if (isCorrect) {
         selectedBtn.classList.add("correct");
+        score++;
     } else {
         selectedBtn.classList.add("incorrect");
     }
+    Array.from(answerButtons.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
 }
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again!";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+});
 
 startQuiz();
